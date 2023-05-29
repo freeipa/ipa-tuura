26c090513494245f0d0d6546390210dd3e01eb36
Francisco Trivino <ftrivino@redhat.com>
2023-05-29 18:30:33 +0200

    [wsgi] Add project path to the sys.path
    When the WSGI server loads, Django needs to import the settings module by using
    the DJANGO_SETTINGS_MODULE environment variable to locate the appropriate settings
    module. It must contain the dotted path to the settings module.
    
    This commit is adding the path to the settings module.
    
    Related: https://github.com/freeipa/ipa-tuura/issues/29
    
    Signed-off-by: Francisco Trivino <ftrivino@redhat.com>

--- src/ipa-tuura/root/wsgi.py
+++ src/ipa-tuura/root/wsgi.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/..")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../root")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "root.settings")
 
 application = get_wsgi_application()
