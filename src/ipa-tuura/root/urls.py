44eff349e2d494099d859b16ffd9680d315cb369
s1341 <github@shmarya.net>
2023-06-14 08:22:04 +0300

    Replace django-rest-swagger with drf-yasg

--- src/ipa-tuura/root/urls.py
+++ src/ipa-tuura/root/urls.py
@@ -19,9 +19,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-schema_view = get_swagger_view(title="Domains API")
+schema_view = get_schema_view(openapi.info(title="Domains API"))
 
 urlpatterns = [
     path("admin/", admin.site.urls),
