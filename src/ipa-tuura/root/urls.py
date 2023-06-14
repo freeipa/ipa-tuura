5b801516715ee81e5a12015f0af02bce30824d57
s1341 <github@shmarya.net>
2023-06-14 09:01:29 +0300

    Fix typo

--- src/ipa-tuura/root/urls.py
+++ src/ipa-tuura/root/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-schema_view = get_schema_view(openapi.info(title="Domains API"))
+schema_view = get_schema_view(openapi.Info(title="Domains API"))
 
 urlpatterns = [
     path("admin/", admin.site.urls),
