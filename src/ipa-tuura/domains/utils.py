freeipa/ipa-tuura

commit 8efb132b69b25c1f89f99028244310176426b5fb

Fix intieroperability issues between domains and scim apps
The domains app serves as an administrative interface, facilitating the addition and removal
of integration domains. On the other hand, the SCIM app is responsible for handling read and
write operations to and from the integration domain. The writability of the interface depends
on specific settings within the integration domain.

This commit fixes interoperability issues by introducing a reset method for the writable
interface. With this implementation, the object is reset after the addition of a new integration
domain, ensuring proper synchronization and functionality.

Fixes: https://github.com/freeipa/ipa-tuura/issues/82

Signed-off-by: Francisco Trivino <ftrivino@redhat.com>

src/ipa-tuura/domains/utils.py
@@ -13,6 +13,7 @@ import ipalib.errors
 import SSSDConfig
 from ipalib import api
 from ipalib.facts import is_ipa_client_configured
+from scim.ipa import IPA
 from scim.models import User
 
 try:
@@ -441,7 +442,6 @@ def add_domain(domain):
 
     Supported identity providers: ipa, ldap, and ad.
     """
-
     # Fail is there's a domain already registered
     try:
         sssdconfig = SSSDConfig.SSSDConfig()
@@ -477,6 +477,10 @@ def add_domain(domain):
     subprocess.run(["sudo", "chmod", "600", "/etc/sssd/sssd.conf"])
     restart_sssd()
     subprocess.run(["sudo", "chmod", "660", "/etc/sssd/sssd.conf"])
+
+    # reset the writable interface
+    ipa = IPA()
+    ipa._reset_instance()
 
 
 def delete_domain(domain):

src/ipa-tuura/domains/views.py
@@ -35,6 +35,8 @@ class DomainViewSet(
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        logger.info(f"domain create {serializer.data}")
+
         try:
             add_domain(serializer.validated_data)
         except RuntimeError as e:

src/ipa-tuura/scim/ipa.py
@@ -198,20 +198,20 @@ class LDAP:
 
     def __init__(self):
         self._conn = None
-        self._dn = None
+        # The client_id is used as the 'Bind DN' object
+        self._client_id = None
+        self._client_secret = None
         self._users_dn = None
         self._ldap_uri = None
         self._ldap_user_extra_attrs = None
+        self._user_rdn_attr = "uid"
         self._user_object_classes = None
         # TLS
         self._ldap_tls_cacert = None
         self._sasl_gssapi = ldap.sasl.sasl({}, "GSSAPI")
-        self._client_id = None
-        self._client_secret = None
         # init and connect
         self._fetch_domain()
         self._conn = self._bind()
-        self._user_rdn_attr = "uid"
 
     def _fetch_domain(self):
         """
@@ -220,13 +220,14 @@ class LDAP:
         domain = domains.models.Domain.objects.last()
         suffix = domain.name.split(".")
 
-        self._dn = domain.client_id
         self._ldap_uri = domain.integration_domain_url
         self._ldap_user_extra_attrs = domain.user_extra_attrs
         self._ldap_tls_cacert = domain.ldap_tls_cacert
         self._client_id = domain.client_id
         self._client_secret = domain.client_secret
         self._users_dn = domain.users_dn
+        if domain.id_provider == "ad":
+            self._user_rdn_attr = "cn"
         self._user_object_classes = [
             x.strip() for x in domain.user_object_classes.split(",")
         ]
@@ -246,7 +247,7 @@ class LDAP:
         self._conn.protocol_version = 3
         self._conn.set_option(ldap.OPT_REFERRALS, 0)
         try:
-            self._conn.simple_bind_s(self._dn, self._client_secret)
+            self._conn.simple_bind_s(self._client_id, self._client_secret)
         except Exception as e:
             logger.error(f"Unable to bind to LDAP server {e}")
             raise e
@@ -407,6 +408,13 @@ class _IPA:
         Initialize writable interface
         """
         self._apiconn = self._write(domains.models.Domain.objects.last().id_provider)
+
+    def _reset_instance(self):
+        """
+        Perform cleanup
+        """
+        self._instance = None
+        self.__init__()
 
     def _write(self, iface="ipa"):
         """

commit 5a706bcb8d1712f8789f790b7691e68b68de5efd

Fix race condition when resetting ipa-writable instance
Commit 8efb132 introduced a regression. This commit fixes a race
condition between domains and scim apps.

Fixes: https://github.com/freeipa/ipa-tuura/issues/85

Signed-off-by: Francisco Trivino <ftrivino@redhat.com>

src/ipa-tuura/domains/utils.py
@@ -13,7 +13,6 @@ import ipalib.errors
 import SSSDConfig
 from ipalib import api
 from ipalib.facts import is_ipa_client_configured
-from scim.ipa import IPA
 from scim.models import User
 
 try:
@@ -477,10 +476,6 @@ def add_domain(domain):
     subprocess.run(["sudo", "chmod", "600", "/etc/sssd/sssd.conf"])
     restart_sssd()
     subprocess.run(["sudo", "chmod", "660", "/etc/sssd/sssd.conf"])
-
-    # reset the writable interface
-    ipa = IPA()
-    ipa._reset_instance()
 
 
 def delete_domain(domain):

src/ipa-tuura/domains/views.py
@@ -17,6 +17,7 @@ from rest_framework.mixins import (
 )
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from scim.ipa import IPA
 
 logger = logging.getLogger(__name__)
 
@@ -35,7 +36,7 @@ class DomainViewSet(
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        logger.info(f"domain create {serializer.data}")
+        logger.info(f"domain create {serializer.validated_data}")
 
         try:
             add_domain(serializer.validated_data)
@@ -45,6 +46,10 @@ class DomainViewSet(
             raise e
         else:
             self.perform_create(serializer)
+
+        # reset the writable interface
+        ipa = IPA()
+        ipa._reset_instance()
 
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
@@ -80,4 +85,5 @@ class DomainViewSet(
             self.perform_destroy(instance)
         except Http404:
             pass
+
         return Response(status=status.HTTP_204_NO_CONTENT)

src/ipa-tuura/scim/ipa.py
@@ -408,12 +408,14 @@ class _IPA:
         Initialize writable interface
         """
         self._apiconn = self._write(domains.models.Domain.objects.last().id_provider)
+        logger.info(f"Init writable interface {self._apiconn}")
 
     def _reset_instance(self):
         """
         Perform cleanup
         """
         self._instance = None
+        logger.info("Reset writable interface")
         self.__init__()
 
     def _write(self, iface="ipa"):